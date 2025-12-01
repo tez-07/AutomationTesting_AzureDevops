66d86767fbe482a7b230c4a8dfdb79a7d7ec1716
Tez Borgohain <tezborgohain1@gmail.com>
2025-12-01 14:07:27 -0500

    Updated code

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -15,12 +15,12 @@ public class BrowserTest {
 	public void getData()
 	{
 		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		 
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://tezborgohain11-gyd0b0a0aaehfsc9.canadacentral-01.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+		Assert.assertTrue(text.equalsIgnoreCase("Azure Devops with Automation"));
 		driver.close();
 	
 		
