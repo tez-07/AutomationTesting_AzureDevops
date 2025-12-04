f097031433cd5e8db0fb43eaa025868adc70b08b
Tez Borgohain <tezborgohain1@gmail.com>
2025-12-03 19:04:02 -0500

    Updated

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 
 public class BrowserTest {
@@ -14,16 +16,15 @@ public class BrowserTest {
 	@Test
 	public void getData()
 	{
-		System.out.println("Hello Guys");
-		 
+		
+		WebDriverManager.chromedriver().setup();
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://tborgohainqa1.azurewebsites.net/webapp/");
+		driver.get("https://tborgohainqa01.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("Azure Devops with Automation"));
 		driver.close();
 	
-		
 		
 	}
 }
