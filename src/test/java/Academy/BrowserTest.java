d53192e8e8ea3cc90a59627a1aedf920a5bde162
Tez Borgohain <tezborgohain1@gmail.com>
2025-12-07 09:35:16 -0500

    Updated code

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -3,6 +3,7 @@ package Academy;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -16,15 +17,14 @@ public class BrowserTest {
 	{
 		
 		ChromeOptions options = new ChromeOptions();
-        ChromeDriver driver = new ChromeDriver(options);
-        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+		options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://tborgohainqa01.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("Azure DevOps with Automation || Updated"));
 		driver.close();
-		
-		//Comments Updated
+	
 		
 	}
 }
